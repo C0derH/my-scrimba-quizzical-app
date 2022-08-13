acf2a6beb63fce087cd86145e4e449879f1d0333
NewbieCoderH <95188463+NewbieCoderH@users.noreply.github.com>
2022-08-13 22:11:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,17 +31,17 @@ export default function App(){
         let randomIndex
       
 
-        if(0 !== currentIndex) {
+
       
 
-          randomIndex = Math.floor(Math.random() * currentIndex)
-          currentIndex -= 1
+       randomIndex = Math.floor(Math.random() * currentIndex)
+       currentIndex -= 1
       
 
-          temporaryValue = array[currentIndex]
-          array[currentIndex] = array[randomIndex]
-          array[randomIndex] = temporaryValue
-        }
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+         array[randomIndex] = temporaryValue
+
       
         return array
     }
